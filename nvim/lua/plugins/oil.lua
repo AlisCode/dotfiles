27ce67b289f69e82cb9dc778bc4ce0730936176e
opinon <oliv.pinon@gmail.com>
2023-05-21 22:46:47 +0200

    Custom keybindings for oil

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,8 +2,20 @@ local oil = require("oil")
 
 oil.setup({
     default_file_explorer = true,
+    columns = {
+        "icon",
+   },
     keymaps = {
-    }
+        ["gp"] = "actions.preview",
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = "actions.select_vsplit",
+        ["<C-h>"] = "actions.select_split",
+        ["<C-j>"] = "actions.select_tab",
+        ["-"] = "actions.parent",
+        ["~"] = "actions.tcd",
+        ["g."] = "actions.toggle_hidden",
+    },
+    use_default_keymaps = false,
 })
 
 function local_file_browser()
