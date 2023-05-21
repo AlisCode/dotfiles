8c5da327de9f39e66e51cdf1cb4eb7c3522179f0
opinon <oliv.pinon@gmail.com>
2023-05-21 22:47:14 +0200

    Fix Omnisharp configuration so that it works on my machine

--- nvim/lua/plugins/lsp/cs.lua
+++ nvim/lua/plugins/lsp/cs.lua
@@ -1,8 +1,15 @@
 -- C# LSP integration through omnisharp
-local vim = vim
 local pid = tostring(vim.fn.getpid())
 local lspconfig = require("lspconfig")
 
+vim.g.OmniSharp_server_path = '/usr/bin/omnisharp'
+
+local on_attach_omnisharp = function(client)
+    -- https://github.com/williamboman/mason-lspconfig.nvim/issues/211
+    client.server_capabilities.semanticTokensProvider = nil
+end
+
 lspconfig.omnisharp.setup({
     cmd = { "omnisharp", "--languageserver", "--hostPID", pid },
+    on_attach = on_attach_omnisharp,
 })
