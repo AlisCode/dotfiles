af61a19f93be86c9af7592987558f74a14567a8a
Olivier Pinon <oliv.pinon@gmail.com>
2023-05-26 16:12:41 +0200

    Remove the useless cmdline when not typing a command

--- nvim/lua/core/settings.lua
+++ nvim/lua/core/settings.lua
@@ -29,3 +29,14 @@ set.backspace = "indent,eol,start"
 set.expandtab = true
 set.smartcase = true
 set.ignorecase = true
+
+-- Hide the useless cmdline by default, and display it
+-- when we're typing a command
+set.cmdheight = 0
+vim.cmd[[
+  augroup hidecmdline
+    autocmd!
+    autocmd CmdlineLeave * set cmdheight=0
+    autocmd CmdlineEnter * set cmdheight=1
+  augroup END
+]]
